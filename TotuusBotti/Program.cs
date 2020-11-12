012a4579e762572da32cef85bd421da7b466117b
Senni Kaipio <senni.kaipio@protonmail.com>
2020-11-12 17:17:27 +0200

    Add RedditBot to get posts from Reddit

--- TotuusBotti/Program.cs
+++ TotuusBotti/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿//#define ENABLE_REDDITBOT
+
+using System;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TotuusBotti
 {
@@ -9,6 +15,8 @@ namespace TotuusBotti
     {
         static ITelegramBotClient botClient;
         static ManualResetEvent quitEvent = new ManualResetEvent(false);
+        static RedditBot redditBot;
+        static Random random = new Random();
 
         static void Main()
         {
@@ -31,6 +39,10 @@ namespace TotuusBotti
 
             Console.WriteLine("Press Ctrl+C to exit");
 
+            redditBot = new RedditBot();
+#if (ENABLE_REDDITBOT)
+            redditBot.Configure();
+#endif
             // Keep program alive until STOP-signal
             quitEvent.WaitOne();
 
@@ -39,15 +51,49 @@ namespace TotuusBotti
 
         static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
-            if (e.Message.Text != null)
-            {
-                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+            if (e.Message.Type != MessageType.Text)
+                return;
 
-                await botClient.SendTextMessageAsync(
-                  chatId: e.Message.Chat,
-                  text: "You said:\n" + e.Message.Text
-                );
-            }
+            Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+            var action = (e.Message.Text.Split(' ').First()) switch
+            {
+                "/pcmeme" => SendPcMeme(e.Message),
+                _ => Usage(e.Message)
+            };
+
+            await action;
+        }
+
+        static async Task SendPcMeme(Message message)
+        {
+            var countOfPostsToGet = 10;
+            // Skip first because it's a moderator post
+            var postIndex = random.Next(1, countOfPostsToGet - 1);
+            var posts = await redditBot.GetPoliticalCompassMemes(countOfPostsToGet);
+            var post = posts.Skip(postIndex).First();
+            var title = post.Title;
+            var postUrl = post.Shortlink; // post.Url.AbsoluteUri;
+            var imageUrl = post.Url.AbsoluteUri;
+
+            await botClient.SendTextMessageAsync(
+              chatId: message.Chat,
+              text: postUrl,
+              disableWebPagePreview: true
+            );
+
+            await botClient.SendTextMessageAsync(
+              chatId: message.Chat,
+              text: title + "\n" + imageUrl
+            );
+        }
+
+        static async Task Usage(Message message)
+        {
+            await botClient.SendTextMessageAsync(
+              chatId: message.Chat,
+              text: "/pcmeme for a random meme from r/politicalcompassmemes"
+            );
         }
     }
 }
---
+++ TotuusBotti/RedditBot.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RedditSharp;
+
+namespace TotuusBotti
+{
+    class RedditBot
+    {
+        private static string accessTokenAddress = "https://www.reddit.com/api/v1/access_token";
+        private string clientId;
+        private string clientSecret;
+        private HttpClient redditHttpClient;
+        private Reddit redditClient;
+        private WebAgent redditWebAgent;
+        private DateTime accessTokenExpires;
+
+        public async void Configure()
+        {
+            clientId = Environment.GetEnvironmentVariable("REDDITTOTUUSBOTTI_CLIENT_ID");
+            clientSecret = Environment.GetEnvironmentVariable("REDDITTOTUUSBOTTI_CLIENT_SECRET");
+            redditHttpClient = new HttpClient();
+            var redditCredentials = Encoding.ASCII.GetBytes(clientId + ":" + clientSecret);
+            var authHeader = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(redditCredentials));
+            redditHttpClient.DefaultRequestHeaders.Authorization = authHeader;
+            redditWebAgent = new WebAgent();
+
+            if (isAccessTokenExpired())
+            {
+                await RefreshAccessToken();
+            }
+
+            redditClient = new Reddit(redditWebAgent, false);
+        }
+
+        public async Task<IEnumerable<RedditSharp.Things.Post>> GetPostsFromSubreddit(string subredditName, int postCount)
+        {
+            // Figure out how to wrap requests so that we don't have to call this every time
+            if (isAccessTokenExpired())
+            {
+                await RefreshAccessToken();
+            }
+            var subreddit = redditClient.GetSubreddit(subredditName);
+            return subreddit.Posts.Take(postCount);
+        }
+
+        public Task<IEnumerable<RedditSharp.Things.Post>> GetPoliticalCompassMemes(int postCount)
+        {
+            return GetPostsFromSubreddit("/r/politicalcompassmemes", postCount);
+        }
+
+        private async Task RefreshAccessToken()
+        {
+            var keyValues = new List<KeyValuePair<string, string>>();
+            keyValues.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            var requestContent = new FormUrlEncodedContent(keyValues);
+            var response = await redditHttpClient.PostAsync(accessTokenAddress, requestContent);
+            var content = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(content);
+            var expiration_time = (int)json.expires_in;
+            // Make expiration 5 second earlier to compensate for request lag
+            accessTokenExpires = DateTime.Now.AddSeconds(expiration_time - 5);
+            redditWebAgent.AccessToken = json.access_token;
+        }
+
+        private bool isAccessTokenExpired()
+        {
+            return DateTime.Now > accessTokenExpires;
+        }
+    }
+}
